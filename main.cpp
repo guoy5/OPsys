43376e642b2ef1be290977b1636fd3710b5fe0be
jocelynchen66 <Jocelyn@folsom-wl-280.dynamic2.rpi.edu>
2017-03-04 17:12:09 -0500

    facts

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ bool readfile(char* file_name, vector<vector<string> >& data){
 	ifstream input;
 	input.open(file_name);
 	if (!input) {
-		cerr << "ERROR: Cannot open txt file" << endl;
+		cerr << "ERROR: Invalid input file format" << endl;
 		return false;
 	}
 	string line;
@@ -37,12 +37,60 @@ bool readfile(char* file_name, vector<vector<string> >& data){
 }
 
 
+void fcfs(const vector<vector<string> >& data){
+	cout << "time 0ms: Simulator started for FCFS [Q <empty>]" << endl;
+	vector<vector<int> > vec;
+	for (int i = 0; i < data.size(); i++){
+		vector<int> tmp;
+		for (int j = 1; j < data[i].size(); j++)
+			tmp.push_back(atoi(data[i][j].c_str()));
+		vec.push_back(tmp);
+	}
+	
+	vector<string> queue;
+
+	while (true){
+		int check = 0;
+		for (int i = 0; i < vec.size(); i++){
+			if (vet[i][2] != 0)
+				check = 1;
+		}
+		if (check = 0) break;
+
+		//************************************
+
+		int min = vec[0][0];
+		for (int i = 0; i < vec.size(); i++){
+			if (vec[i][0] <  min)
+		}
+
+
+
+
+	}
+
+
+
+
+
+	for (int i = 0; i < vec.size(); i++){
+		for (int j = 0; j < vec[i].size(); j++)
+			cout << vec[i][j] << " ";
+		cout << endl;
+	}
+
+
+
+
+}
+
+
 
 
 int main(int argc, char *argv[]){
 
 	if (argc != 2) {
-		cerr << "ERROR: Need two arguments" << endl;
+		cerr << "ERROR: Invalid arguments" << endl;
 		return -1;
 	}
 
@@ -52,11 +100,8 @@ int main(int argc, char *argv[]){
 	if (!readfile(argv[1], data))
 		return -1;
 
-	for (int i = 0; i < data.size(); i++){
-		for (int j = 0; j < data[i].size(); j++)
-			cout << data[i][j] << " ";
-		cout << endl;
-	}
+	fcfs(data);
+
 
 	return 0;
 }
