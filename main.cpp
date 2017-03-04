2bd56ee2f7da1bc053174fddaaee07cd4b9a6558
jocelynchen66 <Jocelyn@folsom-wl-280.dynamic2.rpi.edu>
2017-03-04 16:13:07 -0500

    parse

--- main.cpp
+++ main.cpp
@@ -7,28 +7,59 @@ Contributors: Jing Chen Ying Guo
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
+using namespace std;
 
-
-
-int main(int argc, char*argv[]){
-  if (argc != 2) {
-    std::cout << "Arguments No not correct\n";
-    return EXIT_FAILURE;
-  }
-
-	std::ifstream infile;
-	infile.open(argv[1]);
-	std::string line;
-	if(!infile){
-		fprintf(stderr, "Can not open the file\n" );
-		return EXIT_FAILURE;
+void readfile(char* file_name){
+	ifstream input;
+	input.open(file_name);
+	if (!input) {
+		cerr << "ERROR: Cannot open txt file" << endl;
 	}
 
-	infile.close();
-	
-	
+	string line;
+	vector<vector<string> > data;
 
 
+	while (!input.eof()){
+		getline(input, line);
+		if (line[0] == '#' || line.length() == 0)
+			continue;
+		else {
+		    vector<string> info;
+		    stringstream test(line);
+			string str;
+			while(getline(test, str, '|')){
+			   info.push_back(str);
+			}
+		    data.push_back(info);
+		}
+
+	}
+	input.close();
+
+
+	for (int i = 0; i < data.size(); i++){
+		for (int j = 0; j < data[i].size(); j++)
+			cout << data[i][j] << " ";
+		cout << endl;
+	}
 
 }
 
+
+
+
+int main(int argc, char *argv[]){
+
+	if (argc != 2) {
+		cerr << "ERROR: Need two arguments" << endl;
+		return -1;
+	}
+
+	//Read the txt file and store the information in vectors
+	readfile(argv[1]);
+
+
+	return 0;
+}
