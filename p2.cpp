dddf5cc3260269a9db264ee585b2b4385f8a4a76
jocelynchen66 <Jocelyn@Jings-MBP.lan>
2017-05-01 00:31:24 -0400

    i store arrival time and run time in separate vectors

--- p2.cpp
+++ p2.cpp
@@ -12,6 +12,7 @@ Contributors: Jing Chen(chenj32),Ying Guo(guoy5)
 #include <sstream>
 #include <iomanip>
 #include <algorithm>
+#include <cstdlib>
 
 using namespace std;
 
@@ -45,6 +46,13 @@ bool readfile(char* file_name, vector<vector<string> >& data){
 }
 
 
+void next_fit(vector<vector<string> > data, vector<vector<string> > frame, 
+			  vector<vector<int> > arrival_time, vector<vector<int> > run_time,
+			  ostream& stats_stream){
+	cout << "time 0ms: Simulator started (Contiguous -- Next-Fit)" << endl;
+}
+
+
 int main(int argc, char *argv[]){
 
 	if (argc != 3) {
@@ -73,6 +81,33 @@ int main(int argc, char *argv[]){
 		}
 		frame.push_back(tmp);
 	}
+
+	//Store the arrivial_time and run_time
+	vector<vector<int> > arrival_time;
+	vector<vector<int> > run_time;
+	for (unsigned int i = 1; i < data.size(); i++){
+		vector<int> tmp1;
+		vector<int> tmp2;
+		for (unsigned int j = 0; j < data[i].size(); j++){
+			if (j != 0 && j != 1){
+				stringstream test(data[i][j]);
+				string str;
+				getline(test, str, '/');
+				tmp1.push_back(atoi(str.c_str()));
+				getline(test, str, '/');
+				tmp2.push_back(atoi(str.c_str()));
+			}
+		}
+		arrival_time.push_back(tmp1);
+		run_time.push_back(tmp2);
+	}
+	
+
+	ofstream stat_stream(argv[2]);
+
+	next_fit(data, frame, arrival_time, run_time, stat_stream);
+
+	stat_stream.close();
 
 	return 0;
 }
