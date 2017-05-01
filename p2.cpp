d12ed592d3691c815098055be3611169c153a934
jocelynchen66 <Jocelyn@Jings-MBP.lan>
2017-05-01 01:46:45 -0400

    write some code about next fit, gonna finish it tomorrow

--- p2.cpp
+++ p2.cpp
@@ -46,10 +46,97 @@ bool readfile(char* file_name, vector<vector<string> >& data){
 }
 
 
-void next_fit(vector<vector<string> > data, vector<vector<string> > frame, 
-			  vector<vector<int> > arrival_time, vector<vector<int> > run_time,
+void next_fit(vector<vector<string> > data, vector<vector<string> > frame_, 
+			  vector<vector<int> > arrival_time_, vector<vector<int> > run_time_,
 			  ostream& stats_stream){
+
 	cout << "time 0ms: Simulator started (Contiguous -- Next-Fit)" << endl;
+
+	vector<vector<string> > frame = frame_;
+	vector<vector<int> > arrival_time = arrival_time_;
+	vector<vector<int> > exit_time = run_time_;
+
+	for (unsigned int i = 0; i < exit_time.size(); i++){
+		for (unsigned int j = 0; j < exit_time[i].size(); j++){
+			exit_time[i][j] += arrival_time[i][j];
+		}
+	}
+
+	int total_process = atoi(data[0][0].c_str());
+	vector<string> in_query;
+
+
+	for (unsigned int a = 0; a < 2; a++){
+
+		int curr_process = 0;
+		int min_arrival_time = arrival_time[0][0];
+
+		int curr_exit_process = 0;
+		int min_exit_time = exit_time[0][0];
+
+		for (unsigned int i = 0; i < total_process; i++){
+			if (arrival_time[i][0] < min_arrival_time){
+				min_arrival_time = arrival_time[i][0];
+				curr_process = i;
+			}
+
+			if (exit_time[i][0] < min_exit_time){
+				min_exit_time = arrival_time[i][0];
+				curr_exit_process = i;
+			}
+		}
+
+		if (min_exit_time <= min_arrival_time){
+		}
+
+
+
+		if (min_arrival_time <= min_exit_time){
+
+			// cout << curr_process << endl;
+
+			string curr_letter = data[curr_process+1][0];
+			int fixed_frame = atoi(data[curr_process+1][1].c_str());
+			int arrive_time = arrival_time[curr_process][0];
+		
+			
+			printf("time %dms: Process %s arrived (requires %d frames)\n", arrive_time, curr_letter.c_str(), fixed_frame);
+			printf("time %dms: Placed process %s:\n", arrive_time, curr_letter.c_str());
+
+			in_query.push_back(curr_letter);
+
+			//remove the front element of arrival_time
+			arrival_time[curr_process].erase(arrival_time[curr_process].begin());
+
+			curr_process++;
+
+			int count = 0;
+			for (unsigned int i = 1; i < 9; i++){
+				for (unsigned int j = 0; j < 32; j++){
+					if (count == fixed_frame)
+						break;
+					else{
+						frame[i][j] = data[curr_process][0];
+						count++;
+					}
+				}
+				if (count == fixed_frame)
+					break;
+			}
+
+
+			for (unsigned int i = 0; i < frame.size(); i++){
+				for (unsigned int j = 0; j < frame[i].size(); j++)
+					cout << frame[i][j];
+				cout << endl;
+			}
+		}
+
+	}
+
+
+
+
 }
 
 
@@ -101,7 +188,7 @@ int main(int argc, char *argv[]){
 		arrival_time.push_back(tmp1);
 		run_time.push_back(tmp2);
 	}
-	
+
 
 	ofstream stat_stream(argv[2]);
 
